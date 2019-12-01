48e768c11c95f70e8d6b6c50aa0e6368f97ddff8
Søren Ranneries <spinkelben@gmail.com>
2019-12-01 16:01:36 +0100

    Solve Day1 2019

--- 2019/Year2019Day1.fs
+++ 2019/Year2019Day1.fs
@@ -1,4 +1,24 @@
 ﻿module Year2019Day1
 
-    let main input =
-        (0, 0)
+    let main (input :seq<string>) =
+        let calculateFuel mass = (mass / 3) - 2
+
+        let part1 = 
+            input 
+            |> Seq.map int 
+            |> Seq.map calculateFuel
+            |> Seq.sum
+
+        let part2 = 
+            let rec part2' total newFuel =
+                if newFuel > 0 then
+                    part2' (total + newFuel) (calculateFuel newFuel)
+                else
+                    total
+
+            input 
+            |> Seq.map int 
+            |> Seq.map (fun x -> part2' 0 (calculateFuel x))
+            |> Seq.sum
+
+        (part1, part2)
