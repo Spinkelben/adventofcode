9be0926394b702d5c4de04bd183b75d829a95cd5
Søren Ranneries <spinkelben@gmail.com>
2019-12-10 09:16:25 +0100

    WIP on day 10

---
+++ 2019/Year2019Day10.fs
@@ -0,0 +1,68 @@
+﻿module Year2019Day10
+
+let parseAsteroidMap input = 
+    Seq.mapi (fun yIndex line -> 
+        Seq.mapi (fun xIndex char -> ((xIndex, yIndex), char)) line
+        |> Seq.filter (fun (_, char) -> char = '#'))
+        input
+    |> Seq.concat
+    |> Map.ofSeq
+
+let getAsteroidsInShadow source obstruction asteroidMap mapSize =
+    let getDirection source destination =
+        (fst destination) - (fst source), (snd destination) - (snd source)
+
+    let direction = getDirection source obstruction
+    let xLimit, xDirection = if (fst direction) > 0 then (fst mapSize),1 else 0,-1
+    let yLimit, yDirection = if (snd direction) > 0 then (snd mapSize),1 else 0,-1
+    let mapOfOtherAsteroids = 
+        asteroidMap
+        |> Map.remove source
+        |> Map.remove obstruction 
+    let shadow =
+        match direction with
+        | 0, y -> seq {
+                for yShadow in (snd source) + y .. yDirection .. yLimit ->
+                    (fst source), yShadow
+                }
+        | x, 0 -> seq {
+                for xShadow in (fst source) + x .. xDirection .. xLimit -> 
+                    xShadow, snd(source)
+                }
+        | x, y -> seq {
+                for xShadow in (fst source) + x .. x .. xLimit do
+                    yield! seq {
+                        for yShadow in (snd source) + y .. y .. yLimit do
+                            xShadow, yShadow
+                    }
+                }
+    shadow
+    |> Seq.filter (fun coord -> Map.containsKey coord mapOfOtherAsteroids)
+
+let getHiddenAsteroids source asteroidMap mapSize =
+    Map.remove source asteroidMap
+    |> Map.fold (fun state coord _ -> 
+            let hiddenAsteroids =
+                Set.ofSeq (getAsteroidsInShadow source coord asteroidMap mapSize)
+            Set.union hiddenAsteroids state
+        )
+        Set.empty
+
+let getMapDimensions (input : string seq) =
+    (Seq.head input).Length, Seq.length input
+
+let main input =
+    let xMax, yMax = getMapDimensions input 
+
+    let part1 =
+        let asteroidMap = parseAsteroidMap input
+        asteroidMap 
+        |> Map.map (fun coord _ -> Set.count (getHiddenAsteroids coord asteroidMap (xMax, yMax)))
+        |> Seq.map (fun kvp -> asteroidMap.Count - kvp.Value)
+        |> Seq.max
+
+
+    let part2 =
+        ""
+
+    part1.ToString(), part2
--- AdventOfCodeTests/2019/Year2019Day9Tests.fs
+++ AdventOfCodeTests/2019/Year2019Day9Tests.fs
@@ -17,16 +17,6 @@ let part1Values : obj array seq =
              [1125899906842624L]; |];
     }
 
-let part2Values : obj array seq =
-    seq {
-        yield [| seq { 
-            "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"; };
-             "139629729"; |];
-        yield [| seq { 
-            "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"; };
-             "18216"; |];
-    }
-
 [<Theory>]
 [<MemberData("part1Values")>]
 let ``Part1 Test`` (input :seq<string>, expected) =
@@ -35,9 +25,3 @@ let ``Part1 Test`` (input :seq<string>, expected) =
         |> Array.map int64
     let (output, _, _), _ = IntCodeComputer.executeProgram program [] None None None
     output |> should equal expected
-
-[<Theory>]
-[<MemberData("part2Values")>]
-let ``Part2 Test`` (input :seq<string>, expected) =
-    let _, part2 = Year2019Day7.main input
-    part2 |> should equal expected
