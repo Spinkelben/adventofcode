361b8acd9d67438489b49707e779405651cf9fc7
Søren Ranneries <spinkelben@gmail.com>
2021-12-05 12:29:35 +0100

    Day 5 part 2

--- 2021/Year2021Day5.fs
+++ 2021/Year2021Day5.fs
@@ -26,28 +26,38 @@ module Day5 =
             | (Some p1)::(Some p2)::[] -> Some (p1, p2)
             | _ -> None
 
-        let getPointsInLine (p1, p2) =
-            let getRange x1 x2 =
-                if x1 < x2 then
-                    [x1 .. x2]
-                else 
-                    [x2 .. x1]
+        let getRange x1 x2 =
+            if x1 < x2 then
+                [x1 .. 1 .. x2]
+            else 
+                [x1 .. -1 ..  x2]
 
+        let getPointsInLine (p1, p2) =
             match p1, p2 with
             | (x1, y1),(x2, y2) when x1=x2 -> Some [for i in getRange y1 y2 -> (x1, i)]
             | (x1, y1),(x2, y2) when y1=y2 -> Some [for i in getRange x1 x2 -> (i, y1)]
             | _ -> None
 
-        let part1 = input
-                    |> List.ofSeq
-                    |> List.map readCloudLine // Parse a line into a nested tuple representing the cloud line start and end
-                    |> List.map (Option.bind getPointsInLine) // Expand the vertical and horizontal lines into points
-                    |> List.choose id // Filter out the obligue lines
-                    |> List.concat // Flatten the list of list, we don't care about lines, as we now have the points
-                    |> List.groupBy id // Group by coordinates
-                    |> List.where (fun (coord, list)-> List.length list > 1) // Filter coordinates with less than 2 lines
-                    |> List.length // Count number of coordinates
+        let getPointsInLinePart2 ((x1, y1), (x2, y2)) =
+            let straightLines = getPointsInLine ((x1,y1), (x2, y2))
+            if Option.isSome straightLines then
+                straightLines
+            else 
+                Some (List.zip (getRange x1 x2) (getRange y1 y2))
 
-        let part2 = ""
+        let ventGasFinder lineResolver input =
+            input
+            |> List.ofSeq
+            |> List.map readCloudLine // Parse a line into a nested tuple representing the cloud line start and end
+            |> List.map (Option.bind lineResolver) // Expand the vertical and horizontal lines into points
+            |> List.choose id // Filter out the obligue lines
+            |> List.concat // Flatten the list of list, we don't care about lines, as we now have the points
+            |> List.groupBy id // Group by coordinates
+            |> List.where (fun (coord, list)-> List.length list > 1) // Filter coordinates with less than 2 lines
+            |> List.length // Count number of coordinates
+
+        let part1 = ventGasFinder getPointsInLine input
+
+        let part2 = ventGasFinder getPointsInLinePart2 input
 
         string part1, string part2
