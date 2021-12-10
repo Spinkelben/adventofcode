76f5b15a2e7404ba31f03f90a5f6caca16548fbe
Søren Ranneries <spinkelben@gmail.com>
2021-12-10 19:46:21 +0100

    2021 Day 10 Part2

--- 2021/Year2021Day10.fs
+++ 2021/Year2021Day10.fs
@@ -3,6 +3,10 @@
 open System
 
 module Day10 =
+    type LinePoints =
+        | Incomplete of int64
+        | Corrupted of int
+
     let main (input :string seq) =
         let pointsForCharacter c =
             match c with
@@ -23,17 +27,35 @@ module Day10 =
         let isOpeningTag c =
             List.contains c ['(';'[';'{';'<']
 
+        let getIncompletePoints openChunks =
+            let points = Map.ofList [ (')', 1L); (']', 2L); ('}',3L); ('>', 4L); ]
+            let folder total c = let autocomplete = closingTag c 
+                                 Map.find autocomplete points + total * 5L
+            List.fold folder 0L openChunks
+
         let rec getPointForLine openChunks line =
             match line, openChunks with
-            | [], _ -> 0 // incomplete ignore
+            | [], _ -> getIncompletePoints openChunks |> Incomplete // incomplete ignore
             | c::cs, o::os when closingTag o = c -> getPointForLine os cs  // Matching, chunk closed
             | c::cs, os when isOpeningTag c -> getPointForLine (c :: os) cs // Chunk open
-            | c::cs, _ -> pointsForCharacter c // Mismatch close tag, score the points!
+            | c::_, _ -> pointsForCharacter c |> Corrupted // Mismatch close tag, score the points!
 
         let part1 = input 
                     |> Seq.map (fun line -> getPointForLine [] (List.ofSeq line))
+                    |> Seq.choose (fun x -> match x with 
+                                            | Corrupted x  -> Some x
+                                            | Incomplete _ -> None)
                     |> Seq.sum
             
-        let part2 = ""
+
+        let points = input 
+                    |> Seq.map (fun line -> getPointForLine [] (List.ofSeq line))
+                    |> Seq.choose (fun x -> match x with 
+                                            | Corrupted _  -> None
+                                            | Incomplete x -> Some x)
+                    |> Seq.sort
+
+        let part2 = Seq.item (Seq.length points / 2) points
+                    
             
         string part1, string part2
--- readme.md
+++ readme.md
@@ -34,6 +34,7 @@ Example: `dotnet run 2019 4`
 - Day 7: The Treachery of Whales
 - Day 8: Seven Segment Search
 - Day 9: Smoke Basin
+- Day 10: Syntax Scoring
 
 ### 2020 C#
 
