3f612c99c710fff22ad2a2aba564d5265bde87bd
Søren Ranneries <spinkelben@gmail.com>
2024-12-07 10:31:35 +0100

    Solve day 3 part 2

--- rust/adventofcode/src/puzzle/solution/year2024_day03.rs
+++ rust/adventofcode/src/puzzle/solution/year2024_day03.rs
@@ -25,50 +25,93 @@ impl<'a> MullItOver<'a> {
         Self { input }
     }
 
-    fn scan(&self) -> Vec<MulExpression> {
+    fn scan(&self, use_do_dont: bool) -> Vec<MulExpression> {
         let mut result :Vec<MulExpression> = vec![];
         let mut iter = self.input.chars().peekable();
+        let mut enabled = true;
         while let Some(curr) = iter.next() {
-            if curr != 'm' {
-                continue;
-            }
+            match curr {
+                'm' if enabled => {
+                    let valid = MullItOver::scan_mul(&mut iter);
+                    if !valid {
+                        continue;
+                    }
+                    let left = MullItOver::scan_digits(&mut iter);
+                    if left.is_none() {
+                        continue;
+                    }
+                    let left = left.unwrap().parse::<i32>();
+                    if left.is_err() {
+                        continue;
+                    }
+                    let left = left.unwrap();
 
-            let valid = MullItOver::scan_mul(&mut iter);
-            if !valid {
-                continue;
-            }
-            let left = MullItOver::scan_digits(&mut iter);
-            if left.is_none() {
-                continue;
-            }
-            let left = left.unwrap().parse::<i32>();
-            if left.is_err() {
-                continue;
-            }
-            let left = left.unwrap();
+                    if iter.peek() != Some(&',') {
+                        continue;
+                    }
 
-            if iter.peek() != Some(&',') {
-                continue;
+                    iter.next();
+                    let right = MullItOver::scan_digits(&mut iter);
+                    let right = right.and_then(|r| { r.parse::<i32>().ok() });
+                    if right.is_none() {
+                        continue;
+                    }
+
+                    let right = right.unwrap();
+
+                    if iter.peek() != Some(&')') {
+                        continue;
+                    }
+
+                    iter.next();
+                    result.push(MulExpression { left, right });
+                }
+                'd' if use_do_dont => { // scan do() or don't()
+                    if iter.peek() != Some(&'o') {
+                        continue;
+                    }
+
+                    iter.next();
+                    if MullItOver::scan_parens(&mut iter) {
+                        // scanned "do()"
+                        enabled = true;
+                        continue;
+                    }
+
+                    if iter.next_if_eq(&'n').is_none() {
+                        continue;
+                    }
+
+                    if iter.next_if_eq(&'\'').is_none() {
+                        continue;
+                    }
+
+                    if iter.next_if_eq(&'t').is_none() {
+                        continue;
+                    }
+
+                    if MullItOver::scan_parens(&mut iter) {
+                        // scanned "don't()"
+                        enabled = false;
+                    }
+                }
+                _ => (),
             }
-
-            iter.next();
-            let right = MullItOver::scan_digits(&mut iter);
-            let right = right.and_then(|r| { r.parse::<i32>().ok() });
-            if right.is_none() {
-                continue;
-            }
-
-            let right = right.unwrap();
-
-            if iter.peek() != Some(&')') {
-                continue;
-            }
-
-            iter.next();
-            result.push(MulExpression { left, right });
         }
 
         result
+    }
+
+    fn scan_parens(iterator: &mut Peekable<Chars<'_>>) -> bool {
+        if iterator.next_if_eq(&'(').is_none() {
+            return false;
+        }
+
+        if iterator.next_if_eq(&')').is_none() {
+            return  false;
+        }
+
+        true
     }
 
     fn scan_mul(iterator: &mut Peekable<Chars<'_>>) -> bool {
@@ -106,7 +149,7 @@ impl<'a> MullItOver<'a> {
 
 impl Solution for MullItOver<'_> {
     fn solve_part1(&self) -> String {
-        let mul_instructions = self.scan();
+        let mul_instructions = self.scan(false);
         let result :i32 = mul_instructions
             .iter()
             .map(|e| { e.execute() })
@@ -116,7 +159,13 @@ impl Solution for MullItOver<'_> {
     }
 
     fn solve_part2(&self) -> String {
-        todo!()
+        let mul_instructions = self.scan(true);
+        let result :i32 = mul_instructions
+            .iter()
+            .map(|e| { e.execute() })
+            .sum();
+            
+        result.to_string()
     }
 }
 
@@ -131,7 +180,7 @@ mod test {
     #[test]
     fn parse_mul_test() {
         let solver = MullItOver::new(EXAMPLE);
-        let expressions = solver.scan();
+        let expressions = solver.scan(false);
         assert_eq!(vec![
             MulExpression { left: 2, right: 4 },
             MulExpression { left: 5, right: 5 },
@@ -149,7 +198,7 @@ mod test {
 
     #[test]
     fn part2_test() {
-        let solver = MullItOver::new(EXAMPLE);
+        let solver = MullItOver::new("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
         assert_eq!("48", solver.solve_part2());
     }
 }
