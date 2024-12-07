dc89c14132642d7476fbe662b2bd925b4e93b5b0
Søren Ranneries <spinkelben@gmail.com>
2024-12-07 12:49:40 +0100

    Solve day 4 part 2

--- rust/adventofcode/src/puzzle/solution/year2024_day04.rs
+++ rust/adventofcode/src/puzzle/solution/year2024_day04.rs
@@ -1,7 +1,18 @@
+use std::collections::HashMap;
+
 use super::Solution;
 
 pub struct CeresSearch<'a> {
     input: &'a str,
+}
+
+type Point = (usize, usize);
+
+enum Patterns {
+    Diagonal1(Point),
+    Diagonal2(Point),
+    Vertical(()),
+    Horizontal(())
 }
 
 impl<'a> CeresSearch<'a> {
@@ -23,10 +34,10 @@ impl<'a> CeresSearch<'a> {
             .collect()
     }
 
-    fn search_for_string(string: &Vec<char>, area: &Vec<Vec<char>>) -> usize {
+    fn search_for_string(string: &Vec<char>, area: &[Vec<char>]) -> Vec<Patterns> {
         let mut reversed = string.clone();
         reversed.reverse();
-        let mut num_matches: usize = 0;
+        let mut results = vec![];
         for i in 0..area.len() {
             for j in 0..area[i].len() {
                 // println!("Coords {} {}", i, j);
@@ -36,18 +47,18 @@ impl<'a> CeresSearch<'a> {
                     let horisontal = &area[i][j..j + string.len()];
                     // println!("Horizontal {:#?}", horisontal);
                     if horisontal == string || horisontal == reversed {
-                        num_matches += 1;
+                        results.push(Patterns::Horizontal(()));
                     }
                 }
 
                 if in_bounds_bottom_edge {
                     let mut vertical: Vec<char> = vec![];
-                    for x in i..i + string.len() {
-                        vertical.push(area[x][j]);
+                    for line in area.iter().skip(i).take(string.len()) {
+                        vertical.push(line[j]);
                     }
                     // println!("vertical {:#?}", vertical);
                     if vertical == *string || vertical == reversed {
-                        num_matches += 1;
+                        results.push(Patterns::Vertical(()));
                     }
                 }
 
@@ -58,7 +69,7 @@ impl<'a> CeresSearch<'a> {
                     }
                     // println!("Diagonal1 {:#?}", diagonal1);
                     if diagonal1 == *string || diagonal1 == reversed {
-                        num_matches += 1;
+                        results.push(Patterns::Diagonal1((i, j)));
                     }
     
                     let mut diagonal2: Vec<char> = vec![];
@@ -67,7 +78,7 @@ impl<'a> CeresSearch<'a> {
                     }
                     // println!("Diagonal2 {:#?}", diagonal2);
                     if diagonal2 == *string || diagonal2 == reversed {
-                        num_matches += 1;
+                        results.push(Patterns::Diagonal2((i, j)));
                     }
                 }
 
@@ -75,7 +86,7 @@ impl<'a> CeresSearch<'a> {
             }
         }
 
-        num_matches
+        results
     }
 }
 
@@ -85,11 +96,26 @@ impl Solution for CeresSearch<'_> {
         let search_string = Vec::from_iter("XMAS".chars());
         let num_matches = CeresSearch::search_for_string(&search_string, &input_parsed);
 
-        num_matches.to_string()
+        num_matches.len().to_string()
     }
 
     fn solve_part2(&self) -> String {
-        "".to_string()
+        let input_parsed = self.input_as_2d_array();
+        let seach_string = Vec::from_iter("MAS".chars());
+        let matches = CeresSearch::search_for_string(&seach_string, &input_parsed);
+        let mut map : HashMap<(usize, usize),  Vec<Patterns>> = HashMap::new();
+        for m in matches {
+            match m {
+                Patterns::Diagonal1(p) | 
+                Patterns::Diagonal2(p) => map.entry(p).or_default().push(m),
+                _ => ()
+            }
+        }
+
+        map.iter()
+            .filter(|kvp| kvp.1.len() == 2)
+            .count()
+            .to_string()
     }
 }
 
@@ -109,6 +135,17 @@ mod tests {
     SAXAMASAAA
     MAMMMXMMMM
     MXMXAXMASX";
+
+    const EXAMPLE2: &str = ".M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........";
 
     #[test]
     fn part1_test() {
@@ -168,5 +205,20 @@ mod tests {
                            S....";
         let solver = CeresSearch::new(input);
         assert_eq!("2", solver.solve_part1());
+    }
+
+    #[test]
+    fn simple_part2_test() {
+        let input = "M.M
+                           .A.
+                           S.S";
+        let solver = CeresSearch::new(input);
+        assert_eq!("1", solver.solve_part2());
+    }
+
+    #[test]
+    fn part_2_test() {
+        let solver = CeresSearch::new(EXAMPLE2);
+        assert_eq!("9", solver.solve_part2());
     }
 }
