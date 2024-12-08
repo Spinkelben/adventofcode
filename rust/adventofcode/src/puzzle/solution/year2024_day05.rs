a160a647754657a23eb7465f961c8d72840fadd3
Søren Ranneries <spinkelben@gmail.com>
2024-12-08 14:22:31 +0100

    Solve day 5 part 1

--- rust/adventofcode/src/puzzle/solution/year2024_day05.rs
+++ rust/adventofcode/src/puzzle/solution/year2024_day05.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use std::collections::{HashMap, HashSet};
 
 use super::Solution;
 
@@ -13,6 +13,32 @@ struct  ManualUpdates {
     pages: Vec<usize>
 }
 
+impl  ManualUpdates {
+    fn is_in_right_order(&self, rules: &HashMap<usize, Vec<usize>>) -> bool {
+        // Since both page numbers of the rule has to be present in the manual update for the rule to apply,
+        // it is enough to check the first number
+        let mut past_pages = HashSet::new();
+        for page in &self.pages {
+            let is_rule_broken = rules.get(page)
+                .unwrap_or(&vec![])
+                .iter()
+                .any(|after| past_pages.contains(after));
+            if is_rule_broken {
+                return false;
+            }
+
+            past_pages.insert(page);
+        }
+
+        true
+    }
+    
+    fn get_middle_page(&self) -> usize {
+        self.pages[self.pages.len() / 2]
+    }
+
+}
+
 impl<'a> PrintQueue<'a> {
     pub fn new(input : &'a str) -> Self {
         Self { input }
@@ -20,7 +46,7 @@ impl<'a> PrintQueue<'a> {
 
     fn parse_input(&self) -> Option<(Vec<Rule>, Vec<ManualUpdates>)> {
         let mut parsing_rules = true;
-        let rule_exp = Regex::new(r"(\d+)|(\d+)").unwrap();
+        
         let mut rules = vec![];
         let mut updates = vec![];
         for line in self.input.split("\n")
@@ -31,11 +57,10 @@ impl<'a> PrintQueue<'a> {
                     parsing_rules = false;
                     continue;
                 }
-
-                let Some(captures) = rule_exp.captures(line) else { return  None; };
-                let (_, [before, after]) = captures.extract();
-                let Some(before) = before.parse::<usize>().ok() else { return None; };
-                let Some(after) = after.parse::<usize>().ok() else { return None; };
+                
+                let mut split = line.split("|");
+                let before = split.next()?.parse::<usize>().ok()?;
+                let after = split.next()?.parse::<usize>().ok()?;
                 rules.push((before, after));
             }
             else {
@@ -48,13 +73,28 @@ impl<'a> PrintQueue<'a> {
             }
         }
 
-        return Some((rules, updates));
+        Some((rules, updates))
+    }
+
+    fn create_rule_map(rule_list: &Vec<Rule>) -> HashMap<usize, Vec<usize>> {
+        let mut rule_map: HashMap<usize, Vec<usize>> = HashMap::new();
+        for (before, after) in rule_list {
+            rule_map.entry(*before).or_default().push(*after);
+        }
+
+        rule_map
     }
 }
 
 impl Solution for PrintQueue<'_> {
     fn solve_part1(&self) -> String {
-        todo!()
+        let (rules, updates) = self.parse_input().expect("Failed to parse input");
+        let rule_map = PrintQueue::create_rule_map(&rules);
+        updates.iter()
+            .filter(|u| u.is_in_right_order(&rule_map))
+            .map(|r| r.get_middle_page())
+            .sum::<usize>()
+            .to_string()
     }
 
     fn solve_part2(&self) -> String {
@@ -65,6 +105,8 @@ impl Solution for PrintQueue<'_> {
 
 #[cfg(test)]
 mod tests {
+    use crate::puzzle::solution::Solution;
+
     use super::PrintQueue;
 
     const EXAMPLE : &str = "47|53
@@ -102,8 +144,27 @@ mod tests {
         let solver = PrintQueue::new(EXAMPLE);
         let Some((rules, updates)) = solver.parse_input() else { panic!("Failed to parse!")};
         assert_eq!(21, rules.len());
-        assert_eq!(5, updates.len());
+        assert_eq!(6, updates.len());
         assert_eq!(97, rules[3].0);
-        assert_eq!(97, updates[4].pages[0]);
+        assert_eq!(97, updates[5].pages[0]);
+    }
+
+    #[test]
+    fn valid_update_test() {
+        let solver = PrintQueue::new(EXAMPLE);
+        let Some((rules, updates)) = solver.parse_input() else { panic!("Failed to parse ")};
+        let rules = PrintQueue::create_rule_map(&rules);
+        assert!(updates[0].is_in_right_order(&rules));
+        assert!(updates[1].is_in_right_order(&rules));
+        assert!(updates[2].is_in_right_order(&rules));
+        assert!(!updates[3].is_in_right_order(&rules));
+        assert!(!updates[4].is_in_right_order(&rules));
+        assert!(!updates[5].is_in_right_order(&rules));
+    }
+
+    #[test]
+    fn part1_test() {
+        let solver = PrintQueue::new(EXAMPLE);
+        assert_eq!("143", solver.solve_part1());
     }
 }
